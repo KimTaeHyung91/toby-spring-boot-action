ac3f16a8f7acadce1f3fd3bc8c085564d85d558f
james <dpwe231@gmail.com>
2024-12-04 18:18:01 +0900

    1-1day: Spring Controller 로 작성된 코드를 Servlet 으로작성하여 동일하게 기능하게끔 작성

---
+++ src/main/java/com/example/tobySpringBootAction/HelloBootApplication.java
@@ -0,0 +1,52 @@
+package com.example.tobySpringBootAction;
+
+import java.io.IOException;
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+import org.springframework.boot.web.server.WebServer;
+import org.springframework.boot.web.servlet.ServletContextInitializer;
+import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+
+public class HelloBootApplication {
+
+  public static void main(String[] args) {
+    // Servlet Container
+    TomcatServletWebServerFactory factory = new TomcatServletWebServerFactory();
+    factory.setPort(3000);
+
+    /**
+     * 추상화 시켜놓음
+     * 여러 컨테이너를 자유롭게 사용하기 위해 추상화 -> tomcat 이외의 jetty, netty
+     * 인터페이스가 행위에 초점을 맞춘것도 있지만, 아래 처럼 특정 구현 사항을 의존하지 않게 명사로도 선언될 수 있음을 인지하자
+     *
+     * 웹상에서 스프링 MVC 를 사용을 하는데 이를 Java 의 메소드 또는 파라미터가 요청/응답 사이클에서 어떻게 맵핑해서 사용하는지 주목해서 사용할 필요가 있음
+     * 왜? 웹 API -> Http 를 통해서 API 를 제공하니깐
+     */
+    WebServer webServer = factory.getWebServer(servletContext -> {
+      servletContext.addServlet("hello", new HttpServlet() {
+                      // 요청을 처리하는 서비스 객체
+                      @Override
+                      protected void service(HttpServletRequest req, HttpServletResponse resp)
+                          throws ServletException, IOException {
+                        // 쿼리스트링으로 넘어오는 값 처리
+                        String name = req.getParameter("name");
+
+                        // 웹 응답의 3가지 요소 -> 상태, Content-Type, Body(응답)
+                        resp.setStatus(HttpStatus.OK.value());
+                        resp.setHeader(HttpHeaders.CONTENT_TYPE, MediaType.TEXT_PLAIN_VALUE);
+                        resp.getWriter().println("Hello, " + name);
+                      }
+                    })
+                    // url 맵핑
+                    .addMapping("/hello");
+    });
+    webServer.start();
+  }
+}
