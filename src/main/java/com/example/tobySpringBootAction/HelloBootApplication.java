ed3246813e6986bb45a296aadaaf4deaf67737e7
james <dpwe231@gmail.com>
2024-12-05 18:46:34 +0900

    2-6day: 다시 SpringApplication.run 으로 원복

--- src/main/java/com/example/tobySpringBootAction/HelloBootApplication.java
+++ src/main/java/com/example/tobySpringBootAction/HelloBootApplication.java
@@ -1,13 +1,12 @@
 package com.example.tobySpringBootAction;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
-import org.springframework.boot.web.server.WebServer;
 import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
-import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
 import org.springframework.web.servlet.DispatcherServlet;
 
 // Spring Container 가 빈 구성 정보를 가지고 있는 클래스 라고 인식
@@ -31,27 +30,6 @@ public class HelloBootApplication {
   }
 
   public static void main(String[] args) {
-
-    // Configuration 클래스를 인식하기 위해서는 아래 Context 클래스를 사용
-    AnnotationConfigWebApplicationContext applicationContext = new AnnotationConfigWebApplicationContext() {
-      // TemplateMethod refresh 에서 상속받은 서브 클래스에서 부가적인 로직을 추가 하고 싶을때 아래 hook 메소드에 다가 위치시킨다.
-      @Override
-      protected void onRefresh() {
-        super.onRefresh();
-
-        // Servlet Container
-        ServletWebServerFactory factory = this.getBean(ServletWebServerFactory.class);
-        DispatcherServlet dispatcherServlet = this.getBean(DispatcherServlet.class);
-
-        WebServer webServer = factory.getWebServer(servletContext -> {
-          servletContext.addServlet("dispatcherServlet", dispatcherServlet)
-                        .addMapping("/*");
-        });
-        webServer.start();
-      }
-    };
-    // 해당 Configuration Class 를 등록하여 빈 정보, DI 주입 할 수있게 미리 Context 에 알림
-    applicationContext.register(HelloBootApplication.class);
-    applicationContext.refresh();
+    SpringApplication.run(HelloBootApplication.class, args);
   }
 }
