5253d3bae4eaab788e473fd0d0bb8339bca2d48a
james <dpwe231@gmail.com>
2024-12-05 15:40:01 +0900

    2-2day: 수동으로 된 맵핑 정보를 DispatcherServlet으로 교체

--- src/main/java/com/example/tobySpringBootAction/HelloBootApplication.java
+++ src/main/java/com/example/tobySpringBootAction/HelloBootApplication.java
@@ -12,6 +12,8 @@ import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
+import org.springframework.web.context.support.GenericWebApplicationContext;
+import org.springframework.web.servlet.DispatcherServlet;
 
 public class HelloBootApplication {
 
@@ -22,9 +24,9 @@ public class HelloBootApplication {
      * Configure Metadata: Spring Container로 객체를 등록하기 위한 객체의 메타데이터 등록 e.g) registerBean
      */
     // ApplicationContext(=Spring Container)
-    GenericApplicationContext applicationContext = new GenericApplicationContext();
+    GenericWebApplicationContext applicationContext = new GenericWebApplicationContext();
     applicationContext.registerBean(HelloController.class);
-    // Spring Container 에서 Bean 을 등록 할때 HelloController 에 선언되어있는 HelloService 인터페이스 구현체를 조회하여 알아서 DI 를 넣어줌
+    // Spring Container 에서 Bean 을 등록 할때 HelloController 에 선언되어있는 HelloService 인터페이스 구현체를 조회하여 알아서 DI 를 넣어야
     applicationContext.registerBean(SimpleHelloService.class);
 
     // Spring Container 초기화 -> 등록되어있는 메타데이터를 초기화
@@ -41,41 +43,11 @@ public class HelloBootApplication {
      *
      * 웹상에서 스프링 MVC 를 사용을 하는데 이를 Java 의 메소드 또는 파라미터가 요청/응답 사이클에서 어떻게 맵핑해서 사용하는지 주목해서 사용할 필요가 있음
      * 왜? 웹 API -> Http 를 통해서 API 를 제공하니깐
+     *
+     * DispatcherServlet: URL 맵핑정보를 해당 Servlet 이 웹 요청을 처리 할 컴포넌트를 자동으로 찾아 위임한다.
      */
     WebServer webServer = factory.getWebServer(servletContext -> {
-      servletContext.addServlet("frontController", new HttpServlet() {
-                      // 요청을 처리하는 서비스 객체
-                      @Override
-                      protected void service(HttpServletRequest req, HttpServletResponse resp)
-                          throws ServletException, IOException {
-                        // 요청 URL 을 읽을 수 있는 메소드
-                        String requestURI = req.getRequestURI();
-
-                        /**
-                         * 요청을 받아서 처리하는 과정에서 중요한 적입이 2가지 일어남 -> 맵핑과 바인딩
-                         * 맵핑: 웹 요청의 정보를 활용해서 어떤 로직을 수행할것인가를 결정하는 개념
-                         * e.g) if (requestURI.equals("/hello") && req.getMethod().equals(HttpMethod.GET.name()))
-                         * 위 if 문은 "/hello" 이면서 Get 메소드 일때 어떤 일을 처리하라
-                         *
-                         * 바인딩: 일반적으로 웹 요청을 처리하기 위한 기술적 요소를 오픈하지않음 e.g) HelloController 가 API 요청 시 hello() 메소드를 호출하는가? No
-                         * 즉 요청의 정보를 해석하여 일반적인 Java 코드로 변환해주는것이 바인딩 e.g) req.getParameter("name") -> "/hello?name=Spring"
-                         */
-                        if (requestURI.equals("/hello") && req.getMethod().equals(HttpMethod.GET.name())) {
-                          // 쿼리스트링으로 넘어오는 값 처리
-                          String name = req.getParameter("name");
-                          HelloController helloController = applicationContext.getBean(HelloController.class);
-                          String hello = helloController.hello(name);
-                          // 웹 응답의 3가지 요소 -> 상태, Content-Type, Body(응답)
-                          // resp.setStatus(HttpStatus.OK.value()); -> 생략 가능, 기본적으로 200
-                          resp.setContentType(MediaType.TEXT_PLAIN_VALUE);
-                          resp.getWriter().println(hello);
-                        } else {
-                          resp.setStatus(HttpStatus.NOT_FOUND.value());
-                        }
-                      }
-                    })
-                    // url 맵핑
-                    // '/*' -> frontController: 공통된 사항(보안, 인증, 다국어 처리 응답 형식, 로깅..등)를 요청 핸들러 앞단에서 처리하는 Controller
+      servletContext.addServlet("dispatcherServlet", new DispatcherServlet(applicationContext))
                     .addMapping("/*");
     });
     webServer.start();
