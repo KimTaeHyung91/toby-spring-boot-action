55b894200a4ca6eba6e416b862039802f7bae6b5
james <dpwe231@gmail.com>
2024-12-06 10:10:59 +0900

    3day: TestRestTemplate 을 이용한 테스트 코드 작성

---
+++ src/test/java/com/example/tobySpringBootAction/HelloApiTest.java
@@ -0,0 +1,36 @@
+package com.example.tobySpringBootAction;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+
+public class HelloApiTest {
+
+  @Test
+  void helloApi() {
+    // given
+    // http localhost:8080/hello?name=Spring
+    // 위 엔드포인트 호출을 코드로 작성
+    TestRestTemplate testRestTemplate = new TestRestTemplate();
+
+    // when
+    ResponseEntity<String> res = testRestTemplate.getForEntity(
+        "http://localhost:8080/hello?name={name}",
+        String.class,
+        "Spring");
+
+    // then
+    // status code 200
+    assertThat(res.getStatusCode()).isEqualTo(HttpStatus.OK);
+    // header(content-type) text/plain
+    assertThat(res.getHeaders().getFirst(HttpHeaders.CONTENT_TYPE)).startsWith(MediaType.TEXT_PLAIN_VALUE);
+    // body Hello, Spring
+    assertThat(res.getBody()).isEqualTo("Hello, Spring");
+
+  }
+}
