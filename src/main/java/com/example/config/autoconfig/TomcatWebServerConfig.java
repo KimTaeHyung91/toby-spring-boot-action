cf67c0ac01a2e92dfd2626c9cc7cc02789eda389
james <dpwe231@gmail.com>
2024-12-10 12:49:46 +0900

    5-4day: @Value, PropertySourcesPlaceholderConfigurer 를 이용한 환경 변수 값 할당

--- src/main/java/com/example/config/autoconfig/TomcatWebServerConfig.java
+++ src/main/java/com/example/config/autoconfig/TomcatWebServerConfig.java
@@ -2,22 +2,25 @@ package com.example.config.autoconfig;
 
 import com.example.config.ConditionalMyOnClass;
 import com.example.config.MyAutoConfiguration;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
 import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
 import org.springframework.context.annotation.Bean;
-import org.springframework.core.env.Environment;
 
 // Tomcat 이외의 다른 서버를 사용 할 수 있으니 분리
 @MyAutoConfiguration
 @ConditionalMyOnClass("org.apache.catalina.startup.Tomcat")
 public class TomcatWebServerConfig {
 
+  @Value("${contextPath}")
+  private String contextPath;
+
   @Bean("tomcatWebServerFactory")
   @ConditionalOnMissingBean
-  public ServletWebServerFactory servletWebServerFactory(Environment environment) {
+  public ServletWebServerFactory servletWebServerFactory() {
     TomcatServletWebServerFactory factory = new TomcatServletWebServerFactory();
-    factory.setContextPath(environment.getProperty("contextPath"));
+    factory.setContextPath(this.contextPath);
     return factory;
   }
 }
